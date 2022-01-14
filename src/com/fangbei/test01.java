a13df1c1925a204c1c6b211536862ebdb5bbd98c
FangBei <hello_fangbei@163.com>
2022-01-14 19:26:38 +0800

    test delete out

--- src/com/fangbei/test01.java
+++ src/com/fangbei/test01.java
@@ -3,5 +3,6 @@ package com.fangbei;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("github test");
     }
 }
